2042dc9b708f1d38facf237a95bbc8c497763700
Paco Pronk <paco.pronk@fabrique.nl>
2025-02-08 15:47:52 +0100

    Reset backend to dev branch

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,25 @@
 import express from "express"
 import sequelize from "./services/db.js"
+import cors from "cors"
 import locationApi from "./routes/locationApi.js"
 var app = express()
 
 /* Sync database */
 await sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then(() => {})
   .catch((err) => {
     console.log(err)
   })
+
+/* Global api configurations */
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  })
+)
 
 
 export var clients = []
