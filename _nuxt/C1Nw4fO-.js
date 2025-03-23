d5a32fbb6294e2a01ae69ae943c0bba1d4c05bfb
Paco Pronk <paco.pronk@fabrique.nl>
2025-03-23 16:19:21 +0100

    Updates

---
+++ _nuxt/C1Nw4fO-.js
@@ -0,0 +1,16 @@
+const n=`<?xml version="1.0" encoding="UTF-8"?>
+<svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.51 46.77">
+  <defs>
+    <style>
+      .cls-1 {
+        fill: #fff;
+      }
+    </style>
+  </defs>
+  <g id="Layer_1-2" data-name="Layer 1">
+    <g>
+      <polygon class="cls-1" points="17.01 38.27 17.01 29.76 17.01 21.26 17.01 12.76 8.5 12.76 0 12.76 0 21.26 8.5 21.26 8.5 29.76 8.5 38.27 0 38.27 0 46.77 8.5 46.77 17.01 46.77 25.51 46.77 25.51 38.27 17.01 38.27"/>
+      <rect class="cls-1" x="8.5" width="8.5" height="8.5"/>
+    </g>
+  </g>
+</svg>`;export{n as default};
