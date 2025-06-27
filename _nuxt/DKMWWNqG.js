1e0a9efe77cc54668950f8a6edd316f0a8042606
Paco Pronk <paco.pronk@fabrique.nl>
2025-06-27 09:38:28 +0200

    Updates

---
+++ _nuxt/DKMWWNqG.js
@@ -0,0 +1,4 @@
+const t=`<svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M3.53553 2.46446L30.5355 29.4645M2.46446 29.4645L29.4645 2.46446" stroke="currentColor" stroke-width="5"/>
+</svg>
+`;export{t as default};
